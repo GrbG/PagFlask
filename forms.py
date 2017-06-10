6e3569641f96857c155618636b7ac50c5f7f1968
v4r0 <zero.kullt@gmail.com>
2017-06-10 14:32:06 -0400

    a ver si funciona

---
+++ forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form
+from wtforms import StringField, TextField
+from wtforms.fields.html5 import EmailField
+
+
+class CommentForm(Form):
+    username = StringField('UserName')
+    email = EmailField('Correo Electronico')
+    comment = TextField('Comentario')
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+import forms
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    coment_form = forms.CommentForm()
+    titulo = 'Curso Flask.'
+    return render_template('index.html', title=titulo, form=coment_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
