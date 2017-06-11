879216314ecb11b25e1107d482d6c9e287fbadaa
v4r0 <zero.kullt@gmail.com>
2017-06-11 00:19:19 -0400

    cap 16

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_wtf import CsrfProtect
 import forms  # archivo forms
 
@@ -9,7 +9,7 @@ app.secret_key = 'my_secret_key'  # nadie deberia saber esto xd
 csrf = CsrfProtect(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+"""@app.route('/', methods=['GET', 'POST'])
 def index():
     coment_form = forms.CommentForm(request.form)
     if request.method == 'POST' and coment_form.validate():
@@ -21,6 +21,15 @@ def index():
 
     titulo = 'Curso Flask.'
     return render_template('index.html', title=titulo, form=coment_form)
+"""
+
+
+@app.route('/')
+def index():
+    custom_cookie = request.cookies.get('custome_cookie', 'Undefined')
+    print(custom_cookie)
+    titulo = 'Index'
+    return render_template('index.html', title=titulo)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,6 +37,13 @@ def login():
     login_form = forms.LoginForm(request.form)
     login = 'Login'
     return render_template('login.html', title=login, form=login_form)
+
+
+@app.route('/cookie')
+def cookie():
+    response = make_response(render_template('cookie.html'))
+    response.set_cookie('custom_cookie', 'alvaro')
+    return response
 
 
 if __name__ == '__main__':
