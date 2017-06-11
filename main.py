4e5a1f72142748c7ff97cc2b96d00845c1c0d868
v4r0 <zero.kullt@gmail.com>
2017-06-10 20:15:58 -0400

    cap 12

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import forms  # archivo forms
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    coment_form = forms.CommentForm()
+    coment_form = forms.CommentForm(request.form)
+    if request.method == 'POST':
+        print(coment_form.username.data)
+        print(coment_form.email.data)
+        print(coment_form.comment.data)
     titulo = 'Curso Flask.'
     return render_template('index.html', title=titulo, form=coment_form)
 
